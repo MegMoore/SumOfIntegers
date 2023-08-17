48747c74fb400f0b0a4b0478721bd765018eefe1
Megan Moore <meganmoore.dev@gmail.com>
2023-08-17 14:21:02 -0400

    Add project files.

---
+++ SumOfIntegers/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] nbrs = { 9, 1, 6, 9, 2, 4, 5, 3, 1, 3 };
+
+
+int index = 0;
+
+double sum = nbrs[index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index]
+    + nbrs[++index];
+
+double sumAvg;
+sumAvg = sum / 10;
+Console.WriteLine($"The sum id {sum}");
+Console.WriteLine(sumAvg);
